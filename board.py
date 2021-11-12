bf1c3111fc25c012d797af1b2fb282ed64769850
hongbuly <hongbuly@naver.com>
2021-11-12 12:51:49 +0900

    Add money

--- board.py
+++ board.py
@@ -1,10 +1,9 @@
-from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtWidgets import QLineEdit, QToolButton
+from PyQt5.QtWidgets import QLineEdit, QToolButton, QLabel
 from PyQt5.QtWidgets import QSizePolicy
 from PyQt5.QtWidgets import QLayout, QGridLayout
 
-from functions import *
+from inner import *
 
 
 class Button(QToolButton):
@@ -16,76 +15,114 @@ class Button(QToolButton):
 
     def sizeHint(self):
         size = super(Button, self).sizeHint()
-        size.setHeight(size.height() + 20)
+        size.setHeight(size.height() + 50)
         size.setWidth(max(size.width(), size.height()))
         return size
 
 
 class Board(QWidget):
-    card = []
-    player = []
-    dealer = []
 
     def __init__(self, parent=None):
         super().__init__(parent)
-
+        self.money = load()
+        self.betting_cost = 0
+        # 화면 3개(딜러, 플레이어, 돈)
         self.display = QLineEdit()
         self.display.setReadOnly(True)
-        self.display.setAlignment(Qt.AlignRight)
-        self.display.setMaxLength(15)
+        self.display.setMaxLength(50)
+        self.display1 = QLineEdit()
+        self.display1.setReadOnly(True)
+        self.display1.setMaxLength(50)
+        self.money_display = QLineEdit()
+        self.money_display.setReadOnly(True)
+        self.money_display.setMaxLength(50)
 
         button_layout = QGridLayout()
-        start_button = Button("start", self.buttonClicked)
-        push_button = Button("push", self.buttonClicked)
-        end_button = Button("end", self.buttonClicked)
+        start_button = Button("start", self.button_clicked)
+        push_button = Button("push", self.button_clicked)
+        end_button = Button("end", self.button_clicked)
         button_layout.addWidget(start_button, 0, 1)
         button_layout.addWidget(push_button, 0, 2)
         button_layout.addWidget(end_button, 0, 3)
 
+        betting_layout = QGridLayout()
+        betting_label = QLabel("betting")
+        self.betting_display = QLineEdit()
+        self.betting_display.setMaxLength(50)
+        betting_layout.addWidget(betting_label, 0, 1)
+        betting_layout.addWidget(self.betting_display, 0, 2)
+
         main_layout = QGridLayout()
         main_layout.setSizeConstraint(QLayout.SetFixedSize)
-        main_layout.addWidget(self.display, 0, 0, 1, 2)
-        main_layout.addLayout(button_layout, 1, 0)
+        main_layout.addWidget(self.display1, 0, 0, 1, 2)
+        main_layout.addWidget(self.display, 1, 0, 1, 2)
+        main_layout.addWidget(self.money_display, 3, 0, 1, 2)
+        main_layout.addLayout(betting_layout, 4, 0)
+        main_layout.addLayout(button_layout, 2, 0)
 
         self.setLayout(main_layout)
         self.setWindowTitle("Black Jack")
+        self.money_display.setText('Money:' + str(self.money))
 
-    def buttonClicked(self):
+    def button_clicked(self):
         button = self.sender()
         key = button.text()
         if key == 'start':
-            # 게임 시작, 카드를 랜덤하게 가지고 와서 FIFO 구조로 Player 와 Dealer 에게 번갈아가며 카드 2장 배부
+            betting_cost = self.betting_display.text()
+            if betting_cost.isdigit() and betting_cost:
+                self.betting_cost = int(betting_cost)
+                if self.betting_cost < 1000:
+                    self.display.setText("betting min : 1000")
+                    return
+
+                if self.betting_cost > self.money:
+                    self.display.setText("You don't have much money")
+                    return
+
+                self.betting_display.setReadOnly(True)
+            else:
+                self.display.setText("Write betting number")
+                return
+
+            # 매 게임마다 카드 리셋후, 카드 지급, 내부와 외부 분리
             self.card = set_card()
+            self.player = twocard(self.card)
+            self.dealer = twocard(self.card)
+            # print Gui 에서 안보임
+            print('player')
+            print(show_card(self.player))
+            print('dealer')
+            print(show_card(self.dealer))
+            self.display.setText('player:' + show_card(self.player))
+            self.display1.setText('dealer:' + show_card(self.dealer[1:]) + 'H')
+
+        elif key == 'push':
+            # 카드를 받지 않은 상태에서 push 버튼 클릭시 문구 추가, 내부와 외부 분리
+            cardappend(self.player, self.card)
+            self.display.setText('player:' + show_card(self.player))
+            print(show_card(self.player))
+            if count(self.player) > 21:
+                player_result = count(self.player)
+                dealer_result = count(self.dealer)
+                self.display.setText(fight(player_result, dealer_result))
+
+        elif key == 'end':
+            # 딜러 알고리즘 추가
+            dealer_algo(count(self.dealer), self.dealer, self.card)
+            self.display1.setText('dealer:' + show_card(self.dealer))
+            # 플레이어와 딜러 숫자합 비교, 내부와 외부 분리
+            player_result = count(self.player)
+            dealer_result = count(self.dealer)
+            fight_num = fight(player_result, dealer_result)
+            self.display.setText(get_fight_text(fight_num))
+            self.money = set_money(self.money, self.betting_cost, fight_num)
             self.player.clear()
             self.dealer.clear()
 
-            for i in range(2):
-                self.player.append(self.card.pop(0))
-                self.dealer.append(self.card.pop(0))
-            self.display.setText(show_card(self.player))
-        elif key == 'push':
-            # 카드를 뽑음, @@@ 딜러는 구현 X @@@
-            self.player.append(self.card.pop())
-            self.display.setText(show_card(self.player))
-        elif key == 'end':
-            # 게임 끝, @@@ 딜러가 더 카드를 뽑을 수도 있게 구현해야 함 @@@
-            player_result = count(self.player)
-            dealer_result = count(self.dealer)
+            print(player_result, dealer_result)
 
-            # 승률 조건을 나눔
-            if player_result > 21 and dealer_result > 21:
-                self.display.setText("Draw")
-            elif player_result > 21 and dealer_result <= 21:
-                self.display.setText("Lose")
-            elif player_result <= 21 and dealer_result > 21:
-                self.display.setText("Win")
-            elif player_result <= 21 and dealer_result <= 21:
-                if player_result < dealer_result:
-                    self.display.setText("Lose")
-                elif player_result > dealer_result:
-                    self.display.setText("Win")
-                elif player_result == dealer_result:
-                    self.display.setText("Draw")
+            self.betting_display.setReadOnly(False)
+            self.money_display.setText('Money:' + str(self.money))
 
 
 if __name__ == '__main__':
--- inner.py
+++ inner.py
@@ -1,8 +1,11 @@
 import random
 
+
+marks = ['♠', '◆', '♥', '♣']
+card_english = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+
 # 파일 입출력
-
-
 def load():
     try:
         f = open("money.dat", 'r')
@@ -19,39 +22,52 @@ def write(money):
     f.close()
 
 
+def set_money(now, betting, num):
+    if num == 0:
+        write(str(now - betting))
+        return now - betting
+    elif num == 1:
+        write(str(now + betting))
+        return now - betting
+    else:
+        return now
+
+
 # 카드 두장(베팅 시 카드 지급)지급후에 카드뭉치에서 제거
 def twocard(card):
     cardList = []
     for i in range(2):
-        cardList.append(card.pop())
+        cardList.append(card.pop(0))
     return cardList
 
 
 # 새로운 카드 받기
-
 def cardappend(cardlist, card):
-    cardlist.append(card.pop())
+    cardlist.append(card.pop(0))
 
 
-# end버튼 클릭 이벤트
-
-
+# end 버튼 클릭 이벤트, Lose: 0 Win: 1 Draw 2
 def fight(player_result, dealer_result):
     if player_result > 21:
-        return "Lose, Player Bust"
+        return 0
     elif dealer_result > 21:
-        return "Win, Dealer Bust"
+        return 1
     else:
         if player_result == dealer_result:
-            return "Draw"
+            return 2
         elif player_result < dealer_result:
-            return "Lose"
+            return 0
         elif player_result > dealer_result:
-            return "Win"
+            return 1
 
 
-marks = ['♠', '◆', '♥', '♣']
-card_english = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+def get_fight_text(num):
+    if num == 0:
+        return "Lose"
+    elif num == 1:
+        return "Win"
+    else:
+        return "Draw"
 
 
 def set_card():
@@ -89,5 +105,3 @@ def dealer_algo(result, who, card):
         cardappend(who, card)
         if count(who) > 16:
             break
-
-
