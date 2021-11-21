1e13392fb06050df014552a34a6e2c5160e9c198
hongbuly <60245930+hongbuly@users.noreply.github.com>
2021-11-21 21:57:10 +0900

    Modified 0 < i % 13 < 10

--- board.py
+++ board.py
@@ -108,7 +108,7 @@ class Board(QWidget):
 
             # 블랙잭 체크 추가
             for i in self.player:
-                if 0 < i % 13 < 9:
+                if 0 < i % 13 < 10:
                     break
             else:
                 self.finish_display(True)
